608d34d75063bc96284b25da786d09858382a0d2
vjoseph <valliejoseph@gmail.com>
2015-07-29 19:41:04 +0000

    testing heroku re-name

---
+++ documentation.php
@@ -0,0 +1,22 @@
+ 
+<?php include("header.html");?>
+<body onload="set_style_from_cookie()">
+<center>
+<div class="iconClass">
+	<a class="quickLink" href="documentation2.pdf"><img class="prevCon" src="images/documentation1.png" alt="image goes here">
+	<div class="prevCaption">Instructor and Assistant Documentation</div></a>
+</div>
+<div class="iconClass">
+	<a class="quickLink" href="studentdocumentation.pdf"><img class="prevCon" src="images/documentation1.png" alt="image goes here">
+	<div class="prevCaption">Student Documentation</div></a>
+</div>
+<div class="iconClass">
+	<a class="quickLink" href="documentation3.pdf"><img class="prevCon" src="images/documentation3.PNG" alt="image goes here">
+	<div class="prevCaption">Printer Configuration </div></a>
+</div>
+<div class="iconClass">
+	<a class="quickLink" href="How to Find Remaining Site Licenses.pdf"><img class="prevCon" src="images/documentation4.PNG" alt="image goes here">
+	<div class="prevCaption">Remaining Lisences </div></a>
+</div></center>
+</body>
+<?php include("footer.html");?>
