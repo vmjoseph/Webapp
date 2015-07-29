3634f86844d79d92a1797fe4a63c4622bdaed95d
vjoseph <valliejoseph@gmail.com>
2015-07-29 14:15:54 +0000

    Corrected download to PDO server

--- editSheet.1.php
+++ editSheet.1.php
@@ -26,25 +26,69 @@ $db = new PDO($dsn);
   <table class='viewTimeTable' id='viewTimeTable'>
    <thead>
     <tr>
+     <th class='editClass'>SID</th>
      <th class='editClass'>Last Name</th>
      <th class='editClass'>First Name</th>
+     <th class='editClass'>Mon</th>
+     <th class='editClass'>Tue</th>
+     <th class='editClass'>Wed</th>
+     <th class='editClass'>Thurs</th>
+     <th class='editClass'>Fri</th>
+     <th class='editClass'>Week Total</th>
+     <th class='editClass'>Last Four</th>
      <th class='editClass'>Section Name</th>
-     <th class='editClass'>SID</th>
-     <th class='editClass'>Gender</th>
     </tr>
    </thead>
    <tbody>
 <?php
-$query = "SELECT * FROM students";
+$query = "SELECT * FROM student_hours";
 $result = $db->query($query);
 while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-   echo "<tr class='editClass'>";
-   echo "<td>".$row["first_name"]."</td>";
-   echo "<td>".$row["last_name"]."</td>";
-   echo "<td>".$row["section_type"]."</td>";
-   echo "<td>".$row["sid"]."</td>";
-   echo "<td>".$row["gender"]."</td>";
-   echo "</tr>";
+    echo "<tr class='editClass'>";
+    echo "<td>".$row["sid"]."</td>";
+    echo "<td>".$row["last_name"]."</td>";
+    echo "<td>".$row["first_name"]."</td>";
+    echo "<td>".$row["mon"]."</td>";
+    echo "<td>".$row["tue"]."</td>";
+    echo "<td>".$row["wed"]."</td>";
+    echo "<td>".$row["thurs"]."</td>";
+    echo "<td>".$row["fri"]."</td>";
+    echo "<td>".$row["week_total"]."</td>";
+    echo "<td>".$row["last_four"]."</td>";
+    echo "<td>".$row["section"]."</td>";
+    echo "</tr>";
+    $extractedTable= "<tr class='editClass'>"."<td>".$row["sid"]."</td>"."<td>".$row["last_name"]."</td>"."<td>".$row["first_name"]."</td>"."<td>".$row["mon"]."</td>"."<td>".$row["tue"]."</td>".
+    "<td>".$row["wed"]."</td>"."<td>".$row["thurs"]."</td>". "<td>".$row["fri"]."</td>". "<td>".$row["week_total"]."</td>"."<td>".$row["last_four"]."</td>"."<td>".$row["section"]."</td>"."</tr>";
+}
+echo "</table>";
+$downloadButton = "<form action='' method='GET'>
+    <input type='submit' name='download' class='download' id='download' value='Download Timesheet'>
+    </form>";
+    
+    echo $downloadButton;
+    
+    
+    if(isset($_GET['download'])){
+   
+  
+        ob_clean();
+        $file="timesheet_".date("m/d").".xls";
+        $tableExport="<table class='viewTimeTable' id='viewTimeTable'>".$extractedTable."</table>";
+        header("Content-type: application/vnd.ms-excel");
+        header("Content-Disposition: attachment; filename=$file");
+
+$query = "SELECT * FROM student_hours";
+    //Grabs the result of the query
+    $result = $db->query($query);
+
+    // output data of each row
+    echo "<table class='viewTimeTable' id='viewTimeTable'>";
+    echo "<tr><th class='editClass'>sID</th><th class='editClass'>Last Name </th><th class='editClass'> First Name </th><th class='editClass'> Mon </th> <th class='editClass'> Tue </th> <th class='editClass'> Wed </th> <th class='editClass'> Thurs </th> <th class='editClass'> Fri </th> <th class='editClass'> Week Total </th> <th class='editClass'> Last Four </th> <th class='editClass'> Section </th></tr>";
+   while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        echo "<tr class='editClass'>"."<td>".$row["sid"]."</td>"."<td>".$row["last_name"]."</td>"."<td>".$row["first_name"]."</td>"."<td>".$row["mon"]."</td>"."<td>".$row["tue"]."</td>".
+    "<td>".$row["wed"]."</td>"."<td>".$row["thurs"]."</td>". "<td>".$row["fri"]."</td>". "<td>".$row["week_total"]."</td>"."<td>".$row["last_four"]."</td>"."<td>".$row["section"]."</td>"."</tr>";
+    
+    }echo "</table>";
     
 }
 $result->closeCursor();
