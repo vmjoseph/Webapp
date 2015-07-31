c23d33d250c37d09ff4d193fda001fb54a22b123
vjoseph <valliejoseph@gmail.com>
2015-07-31 20:08:33 +0000

    Locked user login for time being

--- login.php
+++ login.php
@@ -15,15 +15,16 @@ session_start();
 <div id="titleArea"><img src="images/cpcworkspacelogo.png" class="t2"></div></center>
 <center>
 <table class="logTable">
-<tr><td colspan="2"><input type="text" class="field" value="username" onclick='javascript: this.value = ""' name="user"></td></tr>
-<tr><td colspan="2"><input type="password" class="field" onclick='javascript: this.value = ""'  value="password" name="pass"></td></tr>
-<tr><td></td><td colspan="2"><input type="submit" name="login" value="Login"></td></tr>
-<tr><td></td><td colspan="2">Don't have an account?<a href="register.php"> Register Here</a></td></tr>
-<tr><td></td><td colspan="2">Forgot your username/password?<a href="mailto:valliejoseph@gmail.com?Subject=Forgot%20Password&body=Hi,I have forgotten my password to the Workspace App.
- My name is:   and my last remebered login time is: ."> Click Here</a></td></tr>
+<!--<tr><td colspan="2"><input type="text" class="field" value="username" onclick='javascript: this.value = ""' name="user"></td></tr>-->
+<!--<tr><td colspan="2"><input type="password" class="field" onclick='javascript: this.value = ""'  value="password" name="pass"></td></tr>-->
+<!--<tr><td></td><td colspan="2"><input type="submit" name="login" value="Login"></td></tr>-->
+<!--<tr><td></td><td colspan="2">Don't have an account?<a href="register.php"> Register Here</a></td></tr>-->
+<!--<tr><td></td><td colspan="2">Forgot your username/password?<a href="mailto:valliejoseph@gmail.com?Subject=Forgot%20Password&body=Hi,I have forgotten my password to the Workspace App.-->
+<!-- My name is:   and my last remebered login time is: ."> Click Here</a></td></tr>-->
 </table>
 </center>
 </form>
+<h3><center>Hi!<br> This is the example verison of the web app. Currently, the Creator has the website locked and is currently unavailable.<br> Check back later! </center></h3>
 </body>
 </html>
 <?php include("footer.html");?>
