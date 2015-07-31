56cefcf6d038989704ac93859ade4e325f751e3e
vjoseph <valliejoseph@gmail.com>
2015-07-31 12:35:56 +0000

    fixed request password

--- login.php
+++ login.php
@@ -19,7 +19,8 @@ session_start();
 <tr><td colspan="2"><input type="password" class="field" onclick='javascript: this.value = ""'  value="password" name="pass"></td></tr>
 <tr><td></td><td colspan="2"><input type="submit" name="login" value="Login"></td></tr>
 <tr><td></td><td colspan="2">Don't have an account?<a href="register.php"> Register Here</a></td></tr>
-<tr><td></td><td colspan="2">Forgot your username/password?<a href="register.php"> Click Here</a></td></tr>
+<tr><td></td><td colspan="2">Forgot your username/password?<a href="mailto:valliejoseph@gmail.com?Subject=Forgot%20Password&body=Hi,I have forgotten my password to the Workspace App.
+ My name is:   and my last remebered login time is: ."> Click Here</a></td></tr>
 </table>
 </center>
 </form>
